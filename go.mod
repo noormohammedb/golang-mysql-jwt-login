73cdec07379e07abf6d58eeb3dd2271dc7673b86
Noor Mohammed <mail@noormohammed.tech>
2021-11-20 15:05:53 +0530

    Login Completed

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/noormohammedb/golang-mysql-jwt-login
 
 go 1.17
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible
--- handler.go
+++ handler.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt"
 )
 
 var jwtKey = []byte("jwt_secret_key")
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Println("Home")
@@ -17,8 +36,51 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func login(w http.ResponseWriter, r *http.Request) {
 	log.Println("Login")
-	fmt.Fprintf(w, "hello from login")
+	var cred Credentials
+	err := json.NewDecoder(r.Body).Decode(&cred)
+	if err != nil {
+		log.Printf("\nrequest body decode error %v", err)
+		// log.Printf("%#v",err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "error from login")
+		return
+	}
 
+	userPassFromDb, ok := users[cred.Username]
+
+	if !ok || userPassFromDb != cred.Password {
+		log.Print("credentials miss match")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "error from login")
+		return
+	}
+
+	expTime := time.Now().Add(time.Minute * 5)
+
+	newClaim := &Claims{
+		Username: cred.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, newClaim)
+	log.Println(token)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		log.Printf("\ntoken signing error %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "error from login")
+		return
+	}
+	log.Println("signed token : ", tokenString)
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expTime})
+	log.Printf("parsed json %#v \n", cred)
+	fmt.Fprintf(w, "Login Success")
 }
 
 func refresh(w http.ResponseWriter, r *http.Request) {
