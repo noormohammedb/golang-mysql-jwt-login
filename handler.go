e9156e2559f1a694346d2b416a78211c5d4e0ee5
Noor Mohammed <mail@noormohammed.tech>
2021-11-21 12:10:32 +0530

    Refreshing Auth Token

--- handler.go
+++ handler.go
@@ -19,21 +19,7 @@ var users = map[string]string{
 	"user2": "password2",
 }
 
-type Credentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-type TokenClaims struct {
-	Username string `json:"username"`
-	jwt.StandardClaims
-}
-
-type RefreshClaims struct {
-	Username       string `json:"username"`
-	IsRefreshToken bool   `json:"isRefresh"`
-	jwt.StandardClaims
-}
+// var refreshTokensArray map[string]string
 
 func home(w http.ResponseWriter, r *http.Request) {
 	log.Println("Home")
@@ -159,43 +145,80 @@ func login(w http.ResponseWriter, r *http.Request) {
 func refresh(w http.ResponseWriter, r *http.Request) {
 	log.Println("Refresh")
 
-	// cookie, err := r.Cookie("refresh")
-	// if err != nil {
-	// 	// if err == http.ErrNoCookie {
-	// 	// 	log.Println("no token cookie found")
-	// 	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	// 	w.Write([]byte("unauthorized"))
-	// 	// 	return
-	// 	// }
-	// 	log.Println("no cookie found")
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	w.Write([]byte("unauthorized"))
-	// 	return
+	refreshCookie, err := r.Cookie("refresh")
+
+	if err != nil {
+		// if err == http.ErrNoCookie {
+		// 	log.Println("no token cookie found")
+		// 	w.WriteHeader(http.StatusUnauthorized)
+		// 	w.Write([]byte("unauthorized"))
+		// 	return
+		// }
+		log.Println("no cookie found")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("unauthorized"))
+		return
+	}
+
+	refreshTokenString := refreshCookie.Value
+
+	jwtRefreshClaim := &RefreshClaims{}
+
+	jwtRefTokenObj, err := jwt.ParseWithClaims(refreshTokenString, jwtRefreshClaim, func(jwtToken *jwt.Token) (interface{}, error) {
+		return jwtRefKey, nil
+	})
+
+	log.Print("token data ", jwtRefreshClaim)
+
+	// log.Print("jwt token error")
+	// log.Print(jwtTokenObj.Claims.Valid())
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			log.Println("jwt signature invalid")
+		}
+		log.Println("jwt parse error")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("unauthorized"))
+		return
+	}
+
+	// if jwtRefTokenObj.Valid {
+	// 	log.Print("jwt token valid")
+	// 	log.Print(jwtRefTokenObj.Claims.Valid())
+	// 	w.Write([]byte("valid"))
 	// }
 
-	// tokenString := cookie.Value
+	if jwtRefTokenObj.Valid && jwtRefreshClaim.IsRefreshToken {
+		log.Print("refresh token is valid and creating new jwt")
+		expTimeToken := time.Now().Add(time.Minute * 5) // expire after 5 minutes of creation
 
-	// authClaim := &TokenClaims{}
+		newTokenClaim := &TokenClaims{
+			Username: jwtRefreshClaim.Username,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: expTimeToken.Unix(),
+			},
+		}
 
-	// jwtObj, err := jwt.ParseWithClaims(tokenString, authClaim, func(jwtToken *jwt.Token) (interface{}, error) {
-	// 	return jwtTokenKey, nil
-	// })
-	// if err != nil {
-	// 	if err == jwt.ErrSignatureInvalid {
-	// 		log.Println("jwt signature invalid")
-	// 	}
-	// 	log.Println("jwt parse error")
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	w.Write([]byte("unauthorized"))
-	// 	return
-	// }
+		token := jwt.NewWithClaims(jwt.SigningMethodHS512, newTokenClaim)
 
-	// if !jwtObj.Valid {
-	// 	log.Print("jwt token invalid")
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	w.Write([]byte("unauthorized"))
-	// }
+		// log.Printf("%#v", jwtRefreshClaim)
+		tokenString, err := token.SignedString(jwtTokenKey)
+		if err != nil {
+			log.Printf("\ntoken signing error %v", err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "error from refresh")
+			return
+		}
+		http.SetCookie(w,
+			&http.Cookie{
+				Name:    "token",
+				Value:   tokenString,
+				Expires: expTimeToken})
+		fmt.Fprintf(w, "Login Success")
+		return
+	}
 
-	fmt.Fprintf(w, "hello from refresh")
+	fmt.Fprintf(w, "Token Refresh Successfull")
 
 }
---
+++ models.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/golang-jwt/jwt"
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type TokenClaims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+type RefreshClaims struct {
+	Username       string `json:"username"`
+	IsRefreshToken bool   `json:"isRefresh"`
+	jwt.StandardClaims
+}
